2ef1b433b3a49cb7f9a20f64b55f22a188e81016
Gong Zhang <zhanggong@me.com>
2021-11-27 01:12:17 +0800

    Add SwiftUI example target

---
+++ Example Apps/iOS SwiftUI Example/Main.swift
@@ -0,0 +1,17 @@
+//
+//  Main.swift
+//  iOS SwiftUI Example
+//
+//  Created by Gong Zhang on 2021/11/26.
+//
+
+import SwiftUI
+
+@main
+struct Main: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
